e1b39bf1cdd108487e8cc2e95a47ecabc2435fe8
momo-github788 <126054835+momo-github788@users.noreply.github.com>
2023-03-22 19:32:06 +0200

    Removed setters from domain

--- src/main/java/za/ac/cput/domain/Vehicle.java
+++ src/main/java/za/ac/cput/domain/Vehicle.java
@@ -28,57 +28,35 @@ public class Vehicle {
         return vehicleId;
     }
 
-    public void setVehicleId(String vehicleId) {
-        this.vehicleId = vehicleId;
-    }
-
     public String getMake() {
         return make;
     }
 
-    public void setMake(String make) {
-        this.make = make;
-    }
 
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
 
     public String getColour() {
         return colour;
     }
 
-    public void setColour(String colour) {
-        this.colour = colour;
-    }
 
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
 
     public int getMileage() {
         return mileage;
     }
 
-    public void setMileage(int mileage) {
-        this.mileage = mileage;
-    }
 
     @Override
     public String toString() {
